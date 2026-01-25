29de167928a40f69442f8dbee9e8e90ec3fdfcc4
Oleksandr Panichuk <oleksandr.panichuk.dev@gmail.com>
2026-01-25 10:26:25 +0200

    fix: add location to beforeLoad in platform layout route

--- apps/web/src/routes/(platform)/route.tsx
+++ apps/web/src/routes/(platform)/route.tsx
@@ -1,18 +1,18 @@
-import {createFileRoute, redirect} from "@tanstack/react-router";
-import {getSidebarStateFn, PlatformLayout} from "@/features/platform";
-import {ensureCurrentUser} from "@/lib";
+import { createFileRoute, redirect } from "@tanstack/react-router";
+import { getSidebarStateFn, PlatformLayout } from "@/features/platform";
+import { ensureCurrentUser } from "@/lib";
 
 export const Route = createFileRoute("/(platform)")({
 	component: RouteComponent,
-	beforeLoad: async ({ context }) => {
+	beforeLoad: async ({ context, location }) => {
 		const [currentUser, error] = await ensureCurrentUser(context.queryClient);
 
 		if (!currentUser || error) {
 			throw redirect({
 				to: "/sign-in",
 				search: {
-					redirect_url: location.pathname,
-				},
+					redirect_url: location.pathname
+				}
 			});
 		}
 
@@ -20,14 +20,14 @@ export const Route = createFileRoute("/(platform)")({
 			throw redirect({
 				to: "/verification",
 				search: {
-					redirect_url: location.pathname,
-				},
+					redirect_url: location.pathname
+				}
 			});
 		}
 	},
 	loader: async () => {
 		return await getSidebarStateFn();
-	},
+	}
 });
 
 function RouteComponent() {
